579c4b6c9a4a67d7e66ca2eb239a131013601e48
Bill Shannon <bill@billshannon.com>
2014-12-13 16:38:58 -0800

    add faker and validation

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Product, :type => :model do
+  it { should validate_numericality_of(:price).
+                is_greater_than(0.00).
+                is_less_than(1000.0)
+  }
+end
