1a2b826c5f2d5fcece84d6442e3f45fc7b0ffca1
Bill Shannon <bill@billshannon.com>
2014-12-14 16:37:52 -0800

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 validates :price, numericality: {
                   greater_than: 0.00,
-                    less_than: 1000.00
+                    less_than: 2000.00
                 }
 end
