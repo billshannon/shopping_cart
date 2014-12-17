7e1b8f05d555a8fd5001929c7677a76b7cffe969
Bill Shannon <bill@billshannon.com>
2014-12-16 17:51:45 -0700

    tests are green

--- spec/features/creating_products_spec.rb
+++ spec/features/creating_products_spec.rb
@@ -36,7 +36,7 @@ feature 'Product Management' do
     expect(page).to have_text(product.name)
     expect(page).to have_text(product.description)
     expect(page).to have_text(product.price)
-    expect(page).to have_text(product.image_url)
+    expect(page).to have_image(product.image_url.downcase)
   end
 
   scenario 'user views a list of products' do
@@ -48,12 +48,12 @@ feature 'Product Management' do
     expect(page).to have_text(product1.name)
     expect(page).to have_text(product1.description)
     expect(page).to have_text(product1.price)
-    expect(page).to have_text(product1.image_url)
+    expect(page).to have_image(product1.image_url.downcase)
 
     expect(page).to have_text(product2.name)
     expect(page).to have_text(product2.description)
     expect(page).to have_text(product2.price)
-    expect(page).to have_text(product2.image_url)
+    expect(page).to have_image(product2.image_url.downcase)
   end
 
   scenario 'user deletes a product' do
@@ -70,7 +70,7 @@ feature 'Product Management' do
     expect(page).to_not have_text(product.name)
     expect(page).to_not have_text(product.description)
     expect(page).to_not have_text(product.price)
-    expect(page).to_not have_text(product.image_url)
+    expect(page).to_not have_image(product.image_url.downcase)
 
   end
 end
